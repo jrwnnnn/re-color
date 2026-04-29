69e0decead9aea7eeff6c0297a50fe34d7f72937
Mark Jerwin <ping@jrwnnnn.me>
2026-04-29 13:07:33 +0800

    feat: Hello World

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-	<p class="text-lg font-bold">Hello</p>
+	<div
+		class="flex h-screen flex-col items-center justify-center gap-5 px-5 text-center"
+	>
+		<p class="text-4xl font-bold">Welcome to Re:Color v1.0.0!</p>
+		<p class="text-sm text-balance">
+			Re:Color is a standalone digital art application and game specifically
+			designed to be colorblind-friendly. Its core purpose is to bridge the
+			accessibility gap for users with color vision deficiencies by implementing
+			built-in accessibility filters for proper color correction during digital
+			drawing.
+		</p>
+	</div>
 </template>
