777372c8bb078bc8c9731b3d50902d8aad3e359c
Mark Jerwin <ping@jrwnnnn.me>
2026-04-30 11:07:49 +0800

    fix: Remove conditional toggleDevTools menu item

--- main.js
+++ main.js
@@ -33,7 +33,6 @@ function createWindow() {
 			label: "View",
 			submenu: [
 				{ role: "reload" },
-				!app.isPackaged && { role: "toggleDevTools" },
 				{ type: "separator" },
 				{ role: "resetZoom" },
 				{ role: "zoomIn" },
