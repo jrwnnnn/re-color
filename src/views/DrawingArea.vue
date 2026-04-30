ff4af091256c5ea9fa1a8305bace071f4dcc50b6
Mark Jerwin <ping@jrwnnnn.me>
2026-04-30 20:00:39 +0800

    Production Release: v1.1.0 (#58)

---
+++ src/views/DrawingArea.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+import Toolbar from "../components/ToolBar.vue";
+import ColorBar from "../components/ColorBar.vue";
+import StatusBar from "../components/StatusBar.vue";
+import { useCanvasStore } from "../stores/useCanvasStore";
+import { useColorStore } from "../stores/useColorStore";
+import { useStage } from "../lib/useStage";
+import { useTools } from "../lib/useTools";
+import { useKeyboardShortcuts } from "../lib/useKeyboardShortcuts";
+
+const canvasStore = useCanvasStore();
+const colorStore = useColorStore();
+
+const rootRef = ref<HTMLDivElement>();
+const containerRef = ref<HTMLDivElement>();
+
+const { init, destroy, getPos, newCanvas, getStage, getDrawLayer } =
+	useStage(containerRef);
+const { handleMouseDown, handleMouseMove, handleMouseUp } = useTools(
+	getDrawLayer,
+	getPos,
+	canvasStore,
+	colorStore,
+);
+const { onKeyDown } = useKeyboardShortcuts(canvasStore);
+
+onMounted(() => {
+	rootRef.value!.focus();
+	init();
+
+	const stage = getStage();
+	stage.on("mousedown touchstart", handleMouseDown);
+	stage.on("mousemove touchmove", handleMouseMove);
+	stage.on("mouseup touchend", handleMouseUp);
+
+	window.electronAPI?.onNewPainting(() => newCanvas());
+});
+
+onUnmounted(() => destroy());
+</script>
+
+<template>
+	<div
+		ref="rootRef"
+		class="bg-surface flex h-screen flex-col outline-none"
+		tabindex="0"
+		@keydown="onKeyDown"
+	>
+		<div class="flex min-h-0 flex-1 overflow-hidden">
+			<Toolbar />
+			<div class="relative flex-1 overflow-hidden bg-surface">
+				<div ref="containerRef" class="h-full w-full"></div>
+			</div>
+		</div>
+		<ColorBar />
+		<StatusBar />
+	</div>
+</template>
